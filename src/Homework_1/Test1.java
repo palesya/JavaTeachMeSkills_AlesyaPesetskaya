41bb96a7cbcd46acc442970afc7c56dfd8b0982c
”Alesya <palesyav89@gmail.com>
2022-04-05 13:49:13 +0300

    Homework_2 added

--- src/Homework_1/Test1.java
+++ src/Homework_1/Test1.java
@@ -1,5 +1,7 @@
+package Homework_1;
+
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello there");
+        System.out.println("Hello world");
     }
 }
