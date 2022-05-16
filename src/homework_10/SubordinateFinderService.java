ac523d9b9c6cc20c4ad465a633203a408e2f8e18
”Alesya <palesyav89@gmail.com>
2022-05-16 20:01:06 +0300

    SubordinateFinderService changed

--- src/homework_10/SubordinateFinderService.java
+++ src/homework_10/SubordinateFinderService.java
@@ -6,24 +6,27 @@ public class SubordinateFinderService {
 
     private boolean isFound;
 
-    public void find(Director director, String name) {
+    public boolean find(Director director, String name) {
         for (Employee el : director.listOfSubordinates) {
-            if (el instanceof Worker) {
+            if (el.getPosition()==Position.WORKER) {
                 if (Objects.equals(name, el.name)) {
                     System.out.println(String.join(" ", "The subordinate with entered name", name, "is found.\nIt is", el.getFullNameAndPosition()));
                     isFound=true;
+                    break;
                 }
             } else {
                 if (Objects.equals(name, el.name)) {
                     System.out.println(String.join(" ", "The subordinate with entered name", name, "is found.\nIt is", el.getFullNameAndPosition()));
                     isFound=true;
+                    break;
                 }
-                find(((Director) el), name);
+                find((Director) el, name);
             }
         }
         if (!isFound) {
             System.out.println("No subordinates found with name " + name);
         }
+        return isFound;
     }
 
 }
