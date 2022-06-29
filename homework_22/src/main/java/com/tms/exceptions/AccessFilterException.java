acffd83bf2559603ef3ca07fa6a7b42692ef107c
”Alesya <palesyav89@gmail.com>
2022-06-29 19:03:16 +0300

    homework_23

---
+++ homework_22/src/main/java/com/tms/exceptions/AccessFilterException.java
@@ -0,0 +1,4 @@
+package com.tms.exceptions;
+
+public class AccessFilterException extends RuntimeException{
+}
---
+++ homework_22/src/main/java/com/tms/filters/FilterForHeaders.java
@@ -0,0 +1,35 @@
+package com.tms.filters;
+
+import com.tms.exceptions.AccessFilterException;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.*;
+
+@WebFilter(value = "/*")
+public class FilterForHeaders implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("init filter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        List<String> headerNames = new ArrayList<>(Collections.list(httpRequest.getHeaderNames()));
+        String method = httpRequest.getMethod();
+            if ("GET".equalsIgnoreCase(method)||headerNames.contains("Name")) {
+                chain.doFilter(request, response);
+            } else {
+                throw new AccessFilterException();
+            }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy filter");
+    }
+}
