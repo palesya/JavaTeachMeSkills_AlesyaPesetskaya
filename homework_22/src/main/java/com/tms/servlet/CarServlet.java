b9e376e3cff60c13736638f77b5111d85b408214
”Alesya <palesyav89@gmail.com>
2022-07-04 15:05:07 +0300

    homework_23 changed

--- homework_22/src/main/java/com/tms/servlet/CarServlet.java
+++ homework_22/src/main/java/com/tms/servlet/CarServlet.java
@@ -1,6 +1,7 @@
 package com.tms.servlet;
 
 import objects.Car;
+import objects.CarContainer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,21 +11,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
-import java.util.TreeMap;
 
 @WebServlet("/car")
 public class CarServlet extends HttpServlet {
 
-    Map<Integer, Car> cars = new TreeMap<>();
+    CarContainer container = new CarContainer();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        if (!checkIdInRequest(req, resp)) {
+        Map<Integer, Car> cars = container.getCars();
+        if (!container.checkIdInRequest(req, resp)) {
             writer.println("The list of all cars is:");
             cars.forEach((key, value) -> writer.println(key + " : " + value));
         } else {
-            int id = getIdFromRequest(req, resp);
+            int id = container.getIdFromRequest(req, resp);
             if (id >= 0) {
                 writer.println("was found \n" + cars.get(id));
             }
@@ -33,23 +34,17 @@ public class CarServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        if (checkIdInRequest(req, resp)) {
-            writer.println("Id is generated automatically.");
-        }
-        int id = cars.size() + 1;
-        Car car = createCar(req);
-        cars.put(id, car);
-        writer.println("car with id = " + id + " was created: \n" + car);
+        container.saveCar(req, resp);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        if (checkIdInRequest(req, resp)) {
-            int id = getIdFromRequest(req, resp);
+        Map<Integer, Car> cars = container.getCars();
+        if (container.checkIdInRequest(req, resp)) {
+            int id = container.getIdFromRequest(req, resp);
             if (id >= 0) {
-                cars.replace(id, createCar(req));
+                cars.replace(id, container.createCar(req));
                 writer.println("was updated");
             }
         }
@@ -59,8 +54,9 @@ public class CarServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        if (checkIdInRequest(req, resp)) {
-            int id = getIdFromRequest(req, resp);
+        Map<Integer, Car> cars = container.getCars();
+        if (container.checkIdInRequest(req, resp)) {
+            int id = container.getIdFromRequest(req, resp);
             if (id >= 0) {
                 cars.remove(id);
                 writer.println("was removed");
@@ -68,31 +64,4 @@ public class CarServlet extends HttpServlet {
         }
     }
 
-    private Car createCar(HttpServletRequest req) {
-        String brand = req.getParameter("brand");
-        String year = req.getParameter("year");
-        String color = req.getParameter("color");
-        return new Car(brand, year, color);
-    }
-
-    private int getIdFromRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        PrintWriter writer = resp.getWriter();
-        int id = Integer.parseInt(req.getParameter("id"));
-        if (cars.containsKey(id)) {
-            writer.println("Car with id = " + id);
-        } else {
-            writer.println("Car with id = " + id + "\n was not found");
-            id = -1;
-        }
-        return id;
-    }
-
-    private boolean checkIdInRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        boolean isFound = true;
-        if (req.getParameter("id") == null) {
-            resp.getWriter().println("Id is not defined.");
-            isFound = false;
-        }
-        return isFound;
-    }
 }
