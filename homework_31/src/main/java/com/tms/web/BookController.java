8a2081c162dc5e19a20533c7cf068263f355139e
”Alesya <palesyav89@gmail.com>
2022-07-27 12:32:56 +0300

    homework_31 requests and responses are replaced by annotations

--- homework_31/src/main/java/com/tms/web/BookController.java
+++ homework_31/src/main/java/com/tms/web/BookController.java
@@ -4,11 +4,12 @@ import com.tms.model.Book;
 import com.tms.service.impl.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -25,19 +26,18 @@ public class BookController {
     }
 
     @PostMapping
-    public String searchBook(HttpServletRequest req) throws SQLException {
-        String text = req.getParameter("search_text");
-        req.setAttribute("text", text);
+    public String searchBook(
+            @RequestParam(name = "search_text") String text,
+            Model model) throws SQLException {
         List<Book> books = bookService.getBooksByPartialMatch(text);
-        req.setAttribute("books", books);
+        model.addAttribute("books", books);
         return "book";
     }
 
     @PostMapping(path = "/add")
-    public String addBook(HttpServletRequest req) throws SQLException {
-        String name = req.getParameter("book_name");
-        String author = req.getParameter("book_author");
-        bookService.save(new Book(name,author));
+    public String addBook(@RequestParam(name = "book_name")String name,
+                          @RequestParam(name = "book_author")String author) throws SQLException {
+        bookService.save(new Book(name, author));
         return "redirect:/book";
     }
 
