f8becc38679359a6221ba2617da92936db2b6b48
”Alesya <palesyav89@gmail.com>
2022-08-09 13:25:21 +0300

    homework_34 edited

--- homework_34/src/main/java/com/tms/springboot/controller/HomeController.java
+++ homework_34/src/main/java/com/tms/springboot/controller/HomeController.java
@@ -1,12 +1,10 @@
 package com.tms.springboot.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@EnableAutoConfiguration
 @RequestMapping(path = "/")
 public class HomeController {
 
