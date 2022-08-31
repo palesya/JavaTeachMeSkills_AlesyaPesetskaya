93a1fb2db2b3a694c85fa5e6b7513bb742726c35
”Alesya <palesyav89@gmail.com>
2022-08-31 19:46:30 +0300

    homework 40 edited exception classes

--- homework_40/src/main/java/com/tms/homework_40/controller/Handler.java
+++ homework_40/src/main/java/com/tms/homework_40/controller/Handler.java
@@ -1,14 +1,17 @@
 package com.tms.homework_40.controller;
 
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.persistence.EntityNotFoundException;
+
 @RestControllerAdvice
 public class Handler extends Throwable{
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({EntityNotFoundException.class, EmptyResultDataAccessException.class})
     public ResponseEntity<Object> handle(Exception ex) {
         return ResponseEntity.status(400).body(ex.getMessage());
     }
