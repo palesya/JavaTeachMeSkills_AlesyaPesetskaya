5d053176d673531d58a10190e90c98e6dbcf3081
”Alesya <palesyav89@gmail.com>
2022-08-31 19:34:42 +0300

    homework 40 edited

--- homework_40/src/main/java/com/tms/homework_40/controller/Handler.java
+++ homework_40/src/main/java/com/tms/homework_40/controller/Handler.java
@@ -1,17 +1,14 @@
 package com.tms.homework_40.controller;
 
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class Handler extends Throwable{
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handle(Exception ex) {
         return ResponseEntity.status(400).body(ex.getMessage());
     }
