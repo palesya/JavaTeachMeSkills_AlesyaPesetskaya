088180d31c9d28fed2a497fbf72bec288d110155
”Alesya <palesyav89@gmail.com>
2022-08-31 19:26:53 +0300

    homework 40 edited ResponseStatus added

--- homework_40/src/main/java/com/tms/homework_40/controller/Handler.java
+++ homework_40/src/main/java/com/tms/homework_40/controller/Handler.java
@@ -1,14 +1,17 @@
 package com.tms.homework_40.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class Handler extends Throwable{
 
-    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
     public ResponseEntity<Object> handle(Exception ex) {
         return ResponseEntity.status(400).body(ex.getMessage());
     }
