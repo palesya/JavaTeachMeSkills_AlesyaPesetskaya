f34b02efc19fe0b277d12d75dfde79d18e4d08c9
”Alesya <palesyav89@gmail.com>
2022-09-19 20:33:58 +0300

    lecture_46

---
+++ spring-token/src/test/java/com/tms/springtoken/SpringTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.springtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
