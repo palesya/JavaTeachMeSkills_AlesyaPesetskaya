da476f92362ad3eb14c22e33feadaf7078b63624
”Alesya <palesyav89@gmail.com>
2022-09-19 20:31:05 +0300

    lecture_46

--- spring-token/src/main/java/com/tms/springtoken/controller/HomeController.java
+++ spring-token/src/main/java/com/tms/springtoken/controller/HomeController.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<?> get(){
-
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("worked");
     }
 }
